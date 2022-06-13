6e42e327b6890ed0472c81780af776d34886629c
StefanyTP07 <106336762+StefanyTP07@users.noreply.github.com>
2022-06-13 12:40:08 -0500

    Create Proyecto.py

---
+++ Proyecto.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import urllib.request
+import gdown
+
+print('Beginning file download with urllib2...')
+@st.experimental_memo
+def download_data():
+  url = 'https://www.datosabiertos.gob.pe/sites/default/files/Monitoreo_setiembre_Ov.Miraflores.xlsx'
+  filename = 'data.xlsx'
+  urllib.request.urlretrieve(url, 'data.xlsx')
+  return filename
+
+st.write(download_data())
+
+n=st.slider("n", 5,10, step= 1)
+chart_data=pd.DataFrame(np.random.randn(n), columns=['data'])
+st.line_chart(chart_data)
