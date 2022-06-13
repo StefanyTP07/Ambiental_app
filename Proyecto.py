9e9a0714440eb5f65160196dabdb49f2acb3d5e1
StefanyTP07 <106336762+StefanyTP07@users.noreply.github.com>
2022-06-13 12:44:23 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -2,18 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import urllib.request
-import gdown
 
 print('Beginning file download with urllib2...')
+
 @st.experimental_memo
 def download_data():
-  url = 'https://www.datosabiertos.gob.pe/sites/default/files/Monitoreo_setiembre_Ov.Miraflores.xlsx'
-  filename = 'data.xlsx'
-  urllib.request.urlretrieve(url, 'data.xlsx')
+  url = 'https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download'
+  filename = 'data.csv'
+  urllib.request.urlretrieve(url, 'data.csv')
   return filename
+
 
 st.write(download_data())
 
-n=st.slider("n", 5,10, step= 1)
-chart_data=pd.DataFrame(np.random.randn(n), columns=['data'])
+
+n = st.slider("n", 5,100, step=1)
+chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
 st.line_chart(chart_data)
